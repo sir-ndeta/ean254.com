282d23515a621f0da42a3504dbe60829282f5a27
sir-ndeta-github <kerrynw95@gmail.com>
2022-08-02 02:26:53 +0300

    modified:   app.py 	modified:   ean254_com.sql 	new file:   templates/single.html

--- app.py
+++ app.py
@@ -129,6 +129,74 @@ def logout():
     session.pop("key", None)
     return redirect("/login")
 
+import requests
+import datetime
+import base64
+from requests.auth import HTTPBasicAuth
+@app.route('/mpesa', methods = ['POST','GET'])
+def mpesa():
+        if request.method == 'POST':
+            phone = str(request.form['phone'])
+            amount = str(request.form['amount'])
+            # GENERATING THE ACCESS TOKEN
+            consumer_key = "GTWADFxIpUfDoNikNGqq1C3023evM6UH"
+            consumer_secret = "amFbAoUByPV2rM5A"
+
+            api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials" #AUTH URL
+            r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+
+            data = r.json()
+            access_token = "Bearer" + ' ' + data['access_token']
+
+            #  GETTING THE PASSWORD
+            timestamp = datetime.datetime.today().strftime('%Y%m%d%H%M%S')
+            passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+            business_short_code = "174379"
+            data = business_short_code + passkey + timestamp
+            encoded = base64.b64encode(data.encode())
+            password = encoded.decode('utf-8')
+
+
+            # BODY OR PAYLOAD
+            payload = {
+                "BusinessShortCode": "174379",
+                "Password": "{}".format(password),
+                "Timestamp": "{}".format(timestamp),
+                "TransactionType": "CustomerPayBillOnline",
+                "Amount": amount,  # use 1 when testing
+                "PartyA": phone,  # change to your number
+                "PartyB": "174379",
+                "PhoneNumber": phone,
+                "CallBackURL": "https://modcom.co.ke/job/confirmation.php",
+                "AccountReference": "account",
+                "TransactionDesc": "account"
+            }
+
+            # POPULAING THE HTTP HEADER
+            headers = {
+                "Authorization": access_token,
+                "Content-Type": "application/json"
+            }
+
+            url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest" #C2B URL
+
+            response = requests.post(url, json=payload, headers=headers)
+            print (response.text)
+            return("Please complete your payment on the phone")
+        else:
+            return render_template('services.html')
+
+
+@app.route('/single/<SN>')
+def single(SN):
+    conn=pymysql.connect(host='localhost',user='root',password='',database='ean254.com')
+    cursor=conn.cursor()
+
+    sql='select*from services where SN.=%s' 
+    cursor.execute(sql,(SN))
+    row=cursor.fetchone
+    return render_template('single.hml',row=row)
+
 
 
 
